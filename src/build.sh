5ca03c1a4cc852e12858936f9a5c76a24c1b28dd
Alexander Daily <alexanderdaily001@gmail.com>
2023-11-14 20:58:38 +0100

    wip new content

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,11 @@
+# sudo tlmgr install preprint
+# sudo tlmgr install subfiles
+# sudo tlmgr install titlesec
+# sudo tlmgr install enumitem
+# sudo tlmgr install lastpage
+# sudo tlmgr install adjustbox
+
+
 pdflatex -jobname cv main.tex
 rm cv.log
 rm cv.aux
