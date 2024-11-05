51dc4f67f06d7b161df7d3301f8adecdd226bab1
Alexander Daily <alexanderdaily001@gmail.com>
2024-11-05 12:20:43 +0100

    zoi

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,15 @@
+# brew install --cask basictex
+# tlmgr update --self 
 # sudo tlmgr install preprint
 # sudo tlmgr install subfiles
 # sudo tlmgr install titlesec
 # sudo tlmgr install enumitem
 # sudo tlmgr install lastpage
 # sudo tlmgr install adjustbox
-
+# sudo tlmgr install moresize
+# sudo tlmgr install raleway
+# sudo tlmgr install collection-fontsextra
+# sudo tlmgr install lipsum
 
 pdflatex -jobname cv main.tex
 rm cv.log
